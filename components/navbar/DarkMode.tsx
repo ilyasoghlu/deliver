d7577b972753efcffa4d650d3ada3745d48737f0
ilyasoghlu <ilyasoghlu@gmail.com>
2025-10-10 12:03:48 +0400

    dark mode completed

--- components/navbar/DarkMode.tsx
+++ components/navbar/DarkMode.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React from 'react'
-import {MoonIcon, SunIcon} from 'react-icons'
+import {SunIcon, MoonIcon }from '@radix-ui/react-icons' 
 import { Button } from '../ui/button'
 import {useTheme} from  'next-themes'
 import { 
@@ -21,9 +21,21 @@ function DarkMode() {
           size='icon'
           >
             <SunIcon className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0'/>
+            <MoonIcon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100'/>
+            <span className='sr-only'>Toggle theme</span>
           </Button>
-
       </DropdownMenuTrigger>
+      <DropdownMenuContent align='end'>
+        <DropdownMenuItem onClick={() => setTheme('light')}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('dark')}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('system')}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   )
 }
