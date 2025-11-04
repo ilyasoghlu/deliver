0c7e03a079c96943956d418d26364d3ca535c24d
ilyasoghlu <ilyasoghlu@gmail.com>
2025-11-04 17:13:28 +0400

    Blog oage statics

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -9,11 +9,11 @@ function BlogPage() {
             <div className="grid grid-row sm:grid-flow-col grid-col-2 sm:gap-x-4">
                 <div className="col-span-4">
                 <div className="flex flex-col items-start gap-2 p-2">
-                    <div className='h-80 w-3/4'>Image box</div>
-                    <h5>Title </h5>
+                    <div className="h-80 w-3/4">Image box</div>
+                    <h5 className="uppercase text-muted-foreground">7 Strategies to Market Your Business Online</h5>
                     <div className="flex flex-row justify-between gap-6">
                     <div>Date Block</div>
-                    <div>Creater Info</div>
+                    <div>Creator Info</div>
                     <div>News Type</div>
                     <div>Comment Block</div>
                     </div>
@@ -21,21 +21,57 @@ function BlogPage() {
                 </div>
                 <Separator />
                 <div className="flex flex-col items-start gap-2 p-2">
-                    <div>Image box</div>
-                    <h5>Title </h5>
+                    <div className="h-80 w-3/4">Image box</div>
+                    <h5 className="uppercase text-muted-foreground">Basic Post with Featured Image</h5>
                     <div className="flex flex-row justify-between gap-6">
                     <div>Date Block</div>
-                    <div>Creater Info</div>
+                    <div>Creator Info</div>
                     <div>News Type</div>
                     <div>Comment Block</div>
                     </div>
                     <p className="text-muted-foreground mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed nisi eget lectus iaculis congue. Nullam eleifend congue turpis, vel scelerisque massa fermentum ac ... </p>
                 </div>
-                
+                <Separator />
+                <div className="flex flex-col items-start gap-2 p-2 mt-4">
+                    <h5 className="uppercase text-muted-foreground">Basic Post with no Featured Image</h5>
+                    <div className="flex flex-row justify-between gap-6">
+                    <div>Date Block</div>
+                    <div>Creator Info</div>
+                    <div>News Type</div>
+                    <div>Comment Block</div>
+                    </div>
+                    <p className="text-muted-foreground mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed nisi eget lectus iaculis congue. Nullam eleifend congue turpis, vel scelerisque massa fermentum ac ... </p>
+            
+                </div>
+                <Separator />
+                <div className="flex flex-col items-start gap-2 p-2 mt-4">
+                    <div className="h-80 w-3/4">Video Content box</div>
+                    <h5 className="uppercase text-muted-foreground">Video post example</h5>
+                    <div className="flex flex-row justify-between gap-6">
+                    <div>Date Block</div>
+                    <div>Creator Info</div>
+                    <div>News Type</div>
+                    <div>Comment Block</div>
+                    </div>
+                    <p className="text-muted-foreground mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed nisi eget lectus iaculis congue. Nullam eleifend congue turpis, vel scelerisque massa fermentum ac ... </p>
+                </div>
+                <Separator />
+                <div className="flex flex-col items-start gap-2 p-2 mt-4">
+                    <div className="h-20 w-3/4">Audio player box</div>
+                    <h5 className="uppercase text-muted-foreground">Audio clip post example</h5>
+                    <div className="flex flex-row justify-between gap-6">
+                    <div>Date Block</div>
+                    <div>Creator Info</div>
+                    <div>News Type</div>
+                    <div>Comment Block</div>
+                    </div>
+                    <p className="text-muted-foreground mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed nisi eget lectus iaculis congue. Nullam eleifend congue turpis, vel scelerisque massa fermentum ac ... </p>
+                </div>
                 <Separator />
                 <Pagination />
                 </div>
 
+                {/* Sidebar Conetent */}
                 <div className="col-span-1">
                 <div className="py-2">
                     <h6 className="uppercase font-semibold">Popular posts</h6>
