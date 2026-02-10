14fb7ef08de7cea4713720637882d55ccd9e8ba1
ilyasoghlu <ilyasoghlu@gmail.com>
2026-02-10 17:30:28 +0400

    member details page created and information destructed

--- app/about/[id]/page.tsx
+++ app/about/[id]/page.tsx
@@ -1,9 +1,73 @@
 import React from 'react'
 
-function MemberDetailsPage() {
+import { fetchMemberDetails } from '@/utils/actions'
+import Image from 'next/image'
+import { Separator } from '@radix-ui/react-separator'
+
+
+async function MemberDetailsPage({params}:{params:{id:string}}) {
+  const member = await fetchMemberDetails(params.id)
+
+  
+  const { 
+    firstName, 
+    lastName, 
+    description, 
+    position, 
+    image, 
+    experiences = [], 
+    projects = [], 
+    skills = [] 
+  } = member;
+  
+
+  const  [
+    {
+      title: experiencesTitle,
+      company,
+      from,
+      to 
+    } = {},
+  ] = experiences
+
+  const [{
+    title: projectTitle,
+    projectInfo,
+    projectLink
+  } = {}
+] = projects
+
+const [{
+  title: skillsTitle
+} = {}
+] = skills
+
+
   return (
-    <div>MemberDetailsPage</div>
-  )
+    <section className='p-6'>
+      <div className="flex flex-col">
+        {/* Image */}
+        <div className="relative h-full">
+          <Image
+            src={image}
+            alt={firstName}
+            fill
+            sizes=" (max-width:768px) 100vw,
+                        (max-width:1200px) 50vw, 33vw "
+            priority
+            className="w-full rounded object-cover"
+          />
+        </div>
+        {/* Member Content */}
+        <div>
+          <h1 className='font-bold'>{firstName} {lastName}</h1>
+          <h2>{position}</h2>
+          <p>{description}</p>
+        </div>
+      </div>
+      <Separator />
+    </section>
+  );
 }
 
 export default MemberDetailsPage
